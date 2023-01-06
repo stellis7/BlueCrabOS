42acca79f36cfcc9ffb7befb46d431ff04f7a18f
Nick Battista <halodude3675@gmail.com>
2023-01-06 16:26:42 -0500

    created Cargo, copied code
    Copied some basic code off of the "how to make an OS in Rust" website, haven't run or tested yet

---
+++ dev/target/debug/deps/dev-94c3e5f2afdf32dc.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/halod/Documents/GitHub/BlueCrabOS/dev/target/debug/deps/dev-94c3e5f2afdf32dc: src/main.rs
+
+/mnt/c/Users/halod/Documents/GitHub/BlueCrabOS/dev/target/debug/deps/dev-94c3e5f2afdf32dc.d: src/main.rs
+
+src/main.rs:
---
+++ dev/target/debug/dev.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/halod/Documents/GitHub/BlueCrabOS/dev/target/debug/dev: /mnt/c/Users/halod/Documents/GitHub/BlueCrabOS/dev/src/main.rs
