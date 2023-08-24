a7d240439cd25f8cf696b303966d73eb4acecfee
stellis7 <75853555+stellis7@users.noreply.github.com>
2023-08-23 22:04:08 -0400

    Squashed commit of the following:
    commit 98c9684c6bfb0c5ba7d8d8deec0560d9493f390d
    Author: stellis7 <75853555+stellis7@users.noreply.github.com>
    Date:   Wed Aug 23 22:03:15 2023 -0400
    
        Added Lock tests
    
    commit bb4a36185bc19a2b8ce9e51826b2c39d362c4b29
    Author: stellis7 <75853555+stellis7@users.noreply.github.com>
    Date:   Wed Aug 23 21:42:06 2023 -0400
    
        Double Faults Added

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,62 @@
+// in tests/stack_overflow.rs
+
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use blue_crab_os::serial_print;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
+use blue_crab_os::{exit_qemu, QemuExitCode, serial_println};
+use x86_64::structures::idt::InterruptStackFrame;
+
+#[no_mangle]
+pub extern "C" fn _start() ->  ! {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    blue_crab_os::gdt::init();
+    init_test_idt();
+
+    // trigger a stack overflow
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blue_crab_os::test_panic_handler(info)
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow(); // for each recursion, the return address is pushed
+    volatile::Volatile::new(0).read(); // prevent tail recursion optimizations
+}
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(blue_crab_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
+        idt
+    };
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
